5f3cfa8b940e1bac60087e20c8a8d2ecdb7858bd
Jelmer de Ronde <jelmer.deronde@surf.nl>
2021-02-21 16:17:08 +0100

    Change default project

--- {{ cookiecutter.project_name_clean }}/dev/user.clj
+++ {{ cookiecutter.project_name_clean }}/dev/user.clj
@@ -1,17 +1,1 @@
-(ns user
-  (:require [puget.printer :as puget])
-  (:import [java.awt.datatransfer StringSelection]))
-
-(defn clipboard
-  []
-  (.getSystemClipboard (java.awt.Toolkit/getDefaultToolkit)))
-
-(defn clip
-  [data]
-  (let [s (with-out-str (puget/pprint data))]
-    (.setContents (clipboard) (StringSelection. s) nil)))
-
-(add-tap puget/cprint)
-
-(def debug-a (atom nil))
-(add-tap #(reset! debug-a %))
+(ns user)
